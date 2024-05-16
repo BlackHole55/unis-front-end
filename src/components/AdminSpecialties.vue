ae93f7a3938eeacb16c3006b531f8d97d5248fa0
Meirhan <syzdykovmr@gmail.com>
2024-05-16 23:56:00 +0500

    Add Admin Speciality component

---
+++ src/components/AdminSpecialties.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="container pt-3">
+      <h1 class="text-center">Specialties</h1>
+      <!-- <div class="row">
+        <div class="col"></div>
+        <div class="col-8">
+          <form class="form-inline" @submit.prevent="searchSpecialties">
+            <div class="input-group">
+              <input class="form-control" id="search" name="search" v-model="search" type="search" placeholder="Search" aria-label="Search" style="background-color: #008080">
+              <button class="btn btn-outline-dark" id="searchBtn" type="submit">Search</button>
+            </div>
+          </form>
+        </div>
+        <div class="col"></div>
+      </div> -->
+      <div class="row">
+        <div class="col"></div>
+        <div class="col-8">
+            <RouterLink to="specialties/post"><a class="btn mt-3 w-100" id="btn">Post new university</a></RouterLink>
+        </div>
+        <div class="col"></div>
+      </div>
+      <div v-for="speciality in getSpecialties?.specialties?.data" :key="speciality.id">
+          <div class="row">
+            <div class="col"></div>
+            <div class="col-8 mt-3">
+              <div class="card">
+                <div class="card-body">
+                  <div class="card-title d-inline me-3 fw-bold">{{ speciality.name }}</div>
+                  <div class="text-muted">ID: {{ speciality.id }}</div>
+                  <div class="border" id="brdr"></div>
+                  <div class="card-text  pb-2  ">{{ speciality.description }}</div>
+                  <div class="card-footer d-flex">
+                    <button class="btn btn-outline-danger me-auto" @click="deleteSpeciality(speciality.id)">Delete</button>
+                    <a class="btn me-3" id="btn" :href="'specialties/' + speciality.id + '/update'">Edit</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col"></div>
+          </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted, computed } from "vue";
+  import { useStore } from "vuex";
+  
+  export default {
+    setup() {
+      const search = ref('');
+  
+      const store = useStore();
+  
+      const getSpecialties = computed(() => store.getters["specialties/getSpecialties"]);
+    //   const getSearchedUniversities = computed(() => store.getters["universities/getSearchedUniversities"]);
+      
+      console.log(getSpecialties);    
+
+      onMounted(() => {
+        store.dispatch("specialties/fetchSpecialties");
+      });
+
+      function deleteSpeciality(specialityId) {
+        store.dispatch("specialties/deleteSpeciality", specialityId);
+
+        store.dispatch("specialties/fetchSpecialties");
+      }
+
+    //   function searchSpecialties() {
+    //     store.dispatch("universities/search", {
+    //       search: search.value,
+    //     });
+    //     console.log(search.value);
+    //   }
+  
+      return {
+        search,
+        getSpecialties,
+        deleteSpeciality,
+        // searchSpecialties,
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  #searchBtn{
+      border-color: #008080;
+      color: #008080;
+  }
+  #searchBtn:hover{
+      background-color: #008080;
+      color: #fffdd0;
+  }
+  #search {
+    color: #fffdd0;
+  }
+  
+  #btn {
+    background-color: #008080;
+    color: #fffdd0;
+  }
+  #btn:hover {
+    background-color: #fffdd0;
+    color: #008080;
+    border-color: #008080;
+  }
+  
+  .card {
+    background-color: #fffdd0;
+    border-color: #008080;
+  }
+  .card-footer {
+    border-color: #008080;
+  }
+  
+  /* h1 {
+    text-align: center;
+  } */
+  </style>
+  
